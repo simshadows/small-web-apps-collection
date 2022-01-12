02e76fac980aea3f7d33679c6406e4841672e293
simshadows <contact@simshadows.com>
2022-01-12 15:45:48 +1100

    [todo-vanilla-js] Created persistentStore.js

--- web-apps/2021-12-21_todo-app_vanilla-js/index.js
+++ web-apps/2021-12-21_todo-app_vanilla-js/index.js
@@ -1,3 +1,5 @@
+import {DummyPersistentStore} from "./persistentStore.js";
+
 const txt = document.createTextNode.bind(document);
 
 function e(tagName, attributes) {
@@ -5,74 +7,6 @@ function e(tagName, attributes) {
     if ("id" in attributes) elem.setAttribute("id", attributes.id);
     if ("class" in attributes) elem.classList.add(...attributes.class);
     return elem;
-}
-
-function deepcopy(obj) {
-    if (obj === null) return null;
-    switch (typeof obj) {
-        case "undefined":
-        case "boolean":
-        case "number":
-        case "bigint":
-        case "string":
-            return obj;
-        case "object":
-            // Hyperspecific type checking to avoid unintended behaviour.
-            if (obj.constructor === Map) {
-                return new Map(obj);
-            } else if (obj.constructor === Array) {
-                return [...obj];
-            } else if (obj.constructor === Object) {
-                return Object.assign({}, obj); // Only copies enumerable own properties.
-            }
-            throw "Unsupported prototype.";
-        default:
-            throw "Unsupported typeof value.";
-    }
-}
-
-class DummyPersistentStore {
-    constructor() {
-        this._collections = new Map([
-            [0, {
-                name: "My awesome list!",
-                todos: new Map([
-                    [0, {
-                        done: false,
-                        title: "Foo",
-                    }],
-                    [1, {
-                        done: true,
-                        title: "Bar",
-                    }],
-                    [2, {
-                        done: false,
-                        title: "Baz",
-                    }],
-                ]),
-                lastUnusedTodoID: 3,
-            }],
-            [1, {
-                name: "Empty List",
-                todos: new Map(), // Empty
-                lastUnusedTodoID: 0,
-            }],
-        ]);
-        this._nextUnusedID = 3;
-    }
-    
-    read() {
-        console.log("dummy store read");
-        return {
-            collections: deepcopy(this._collections),
-            nextUnusedID: this._nextUnusedID,
-        };
-    }
-    write(obj) {
-        console.log("dummy store write");
-        this._collections = deepcopy(obj.collections);
-        this._nextUnusedID = obj.nextUnusedID;
-    }
 }
 
 function addButtonElement(onClickHandler) {
@@ -137,7 +71,7 @@ function todoBoxElement(collectionID, todoID, todoData) {
 }
 
 function collectionDetailBodyElement(collectionData) {
-    elem = e("div", {id: "app-body", class: ["collection-detail-body"]});
+    const elem = e("div", {id: "app-body", class: ["collection-detail-body"]});
     for (const [todoID, todoData] of collectionData.todos.entries()) {
         elem.appendChild(todoBoxElement(
             collectionData.id,
@@ -314,7 +248,9 @@ const state = {
     view: "collections_overview", // "collections_overview" | "collection_detail"
     openCollectionID: null,
 };
+
 let data = persistentStore.read(); // {collections, nextUnusedID}
+                                   // SEE persistentStore.js for an example of this data structure.
 
 render();
 

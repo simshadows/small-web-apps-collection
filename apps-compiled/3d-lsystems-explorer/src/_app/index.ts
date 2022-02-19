158847d978a4a43462f172601ec3b8dc536bd8df
simshadows <contact@simshadows.com>
2022-02-20 00:44:19 +1100

    [3dlse] Canvas now fills the window

--- apps-compiled/3d-lsystems-explorer/src/_app/index.ts
+++ apps-compiled/3d-lsystems-explorer/src/_app/index.ts
@@ -2,32 +2,37 @@ import "regenerator-runtime/runtime";
 
 import * as THREE from "three";
 
-// init
+import "./index.css";
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const geometry = new THREE.BoxGeometry(0.5, 0.2, 0.2);
 const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
 
-// animation
+function resizeCanvas() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
 
 function animation(time: number) {
+    resizeCanvas();
 
     mesh.rotation.x = time / 2000;
     mesh.rotation.y = time / 1000;
     
-    renderer.render( scene, camera );
-
+    renderer.render(scene, camera);
 }
 

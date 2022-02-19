7023183b30d05932663184aca605a0e4b418b520
simshadows <contact@simshadows.com>
2022-02-20 09:49:19 +1100

    [3dlse] Added a few more geometries to test the graphics library

--- apps-compiled/3d-lsystems-explorer/src/_app/index.ts
+++ apps-compiled/3d-lsystems-explorer/src/_app/index.ts
@@ -4,16 +4,44 @@ import * as THREE from "three";
 
 import "./index.css";
 
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-camera.position.z = 1;
+function getCustomMesh() {
+    const points = [
+        new THREE.Vector3(-1, 0, 0),
+        new THREE.Vector3(0, 1, 0),
+        new THREE.Vector3(1, 0, 0),
+    ];
+    return new THREE.Mesh(
+        new THREE.BufferGeometry().setFromPoints(points),
+        new THREE.LineBasicMaterial({color: 0x0000ff}),
+    );
+}
+
+function getSimpleCylinder() {
+    const radius = 0.1;
+    return new THREE.Mesh(
+        new THREE.CylinderGeometry(radius, radius, 2.0, 20),
+        new THREE.MeshNormalMaterial(),
+    );
+}
+
+function getSimpleBox() {
+    return new THREE.Mesh(
+        new THREE.BoxGeometry(1.0, 0.4, 0.4),
+        new THREE.MeshNormalMaterial(),
+    );
+}
+
+const meshes = [
+    getSimpleBox(),
+    getCustomMesh(),
+    getSimpleCylinder(),
+];
+
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 10);
+camera.position.z = 2;
 
 const scene = new THREE.Scene();
-
-const geometry = new THREE.BoxGeometry(0.5, 0.2, 0.2);
-const material = new THREE.MeshNormalMaterial();
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+for (const mesh of meshes) scene.add(mesh);
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setAnimationLoop(animation);
@@ -30,8 +58,10 @@ function resizeCanvas() {
 function animation(time: number) {
     resizeCanvas();
 
-    mesh.rotation.x = time / 2000;
-    mesh.rotation.y = time / 1000;
+    for (const mesh of meshes) {
+        mesh.rotation.x = time / 2000;
+        mesh.rotation.y = time / 1000;
+    }
     
     renderer.render(scene, camera);
 }

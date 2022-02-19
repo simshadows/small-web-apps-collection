f00e54e0cd142cdfafcac89dbe6ef3bceb7b7e76
simshadows <contact@simshadows.com>
2022-02-20 10:27:19 +1100

    [3dlse] The camera now auto-rotates instead of the meshes

--- apps-compiled/3d-lsystems-explorer/src/_app/index.ts
+++ apps-compiled/3d-lsystems-explorer/src/_app/index.ts
@@ -5,50 +5,20 @@ import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 import "./index.css";
 
-function getCustomMesh() {
-    const points = [
-        new THREE.Vector3(-1, 0, 0),
-        new THREE.Vector3(0, 1, 0),
-        new THREE.Vector3(1, 0, 0),
-    ];
-    return new THREE.Mesh(
-        new THREE.BufferGeometry().setFromPoints(points),
-        new THREE.LineBasicMaterial({color: 0x0000ff}),
-    );
-}
-
 function getSimpleCylinder() {
     const radius = 0.1;
     return new THREE.Mesh(
-        new THREE.CylinderGeometry(radius, radius, 2.0, 20),
+        new THREE.CylinderGeometry(radius, radius, 10.0, 20),
         new THREE.MeshNormalMaterial(),
     );
 }
 
 function getSimpleBox() {
     return new THREE.Mesh(
-        new THREE.BoxGeometry(1.0, 0.4, 0.4),
+        new THREE.BoxGeometry(2.0, 0.8, 0.8),
         new THREE.MeshNormalMaterial(),
     );
 }
-
-const meshes = [
-    getSimpleBox(),
-    getCustomMesh(),
-    getSimpleCylinder(),
-];
-
-const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 10);
-camera.position.z = 2;
-
-const scene = new THREE.Scene();
-for (const mesh of meshes) scene.add(mesh);
-
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setAnimationLoop(animation);
-document.body.appendChild(renderer.domElement);
-
-new OrbitControls(camera, renderer.domElement);
 
 function resizeCanvas() {
     const width = window.innerWidth;
@@ -58,14 +28,28 @@ function resizeCanvas() {
     camera.updateProjectionMatrix();
 }
 
-function animation(time: number) {
+function animation() {
+    controls.update();
     resizeCanvas();
-
-    for (const mesh of meshes) {
-        mesh.rotation.x = time / 2000;
-        mesh.rotation.y = time / 1000;
-    }
-    
     renderer.render(scene, camera);
 }
+
+const meshes = [
+    getSimpleBox(),
+    getSimpleCylinder(),
+];
+
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 100);
+camera.position.z = 10;
+
+const scene = new THREE.Scene();
+for (const mesh of meshes) scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
 

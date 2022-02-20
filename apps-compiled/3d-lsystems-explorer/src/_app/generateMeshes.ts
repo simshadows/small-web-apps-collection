65b1e84dc640d7dd6348ed4ccc69f2c8144f7e2f
simshadows <contact@simshadows.com>
2022-02-20 15:51:31 +1100

    [3dlse] Added optimizations for straight segments and poly detail

--- apps-compiled/3d-lsystems-explorer/src/_app/generateMeshes.ts
+++ apps-compiled/3d-lsystems-explorer/src/_app/generateMeshes.ts
@@ -12,10 +12,10 @@ function degToRad(deg: number): number {
     return deg * (Math.PI/180);
 }
 
-function getSimpleLine(start: THREE.Vector3, end: THREE.Vector3, thickness: number) {
+function getSimpleLine(start: THREE.Vector3, end: THREE.Vector3, thickness: number, radialSegments: number) {
     const path = new THREE.LineCurve3(start, end);
     return new THREE.Mesh(
-        new THREE.TubeGeometry(path, 1, thickness, 5, false), // I have no idea why I can't set closed to true
+        new THREE.TubeGeometry(path, 1, thickness, radialSegments, false), // I have no idea why I can't set closed to true
         new THREE.MeshNormalMaterial(),
     );
 }
@@ -35,6 +35,7 @@ export function generateMeshes() {
     let base = new THREE.Vector3(0, 0, 0);
     let direction = new THREE.Vector3(0, 1, 0);
     let thickness = 1.2;
+    let radialSegments = 10;
     let stack: {base: THREE.Vector3; direction: THREE.Vector3, thickness: number}[] = [];
 
     function push(): void {
@@ -51,28 +52,47 @@ export function generateMeshes() {
         thickness = popped.thickness;
     }
 
-    function draw(): void {
-        const end = base.clone().add(direction);
-        meshes.push(getSimpleLine(base, end, thickness));
+    function draw(length: number): void {
+        const end = direction.clone().multiplyScalar(length).add(base);
+        meshes.push(getSimpleLine(base, end, thickness, radialSegments));
         base = end;
     }
     function rotate(angleDeg: number, axis: THREE.Vector3): void {
         direction.applyAxisAngle(axis, degToRad(angleDeg));
         thickness *= 0.95;
+        radialSegments = Math.max(3, radialSegments - 1);
     }
 
+    let currSegmentLength = 0;
     for (const s of sequence) {
+
+        // Optimization to lump multiple consecutive draw commands into one straight mesh.
+        if (s === "F") {
+            ++currSegmentLength;
+            continue;
+        } else if (currSegmentLength > 0) {
+            draw(currSegmentLength);
+            currSegmentLength = 0;
+        }
+
         switch (s) {
             case "[": push(); break;
             case "]": pop();  break;
-            case "F": draw(); break;
             case "+": rotate(30, new THREE.Vector3(1, 0, 0)); break;
             case "-": rotate(-30, new THREE.Vector3(1, 0, 0)); break;
             case "*": rotate(30, new THREE.Vector3(0, 1, 0)); break;
             case "/": rotate(-30, new THREE.Vector3(0, 1, 0)); break;
+
+            //case "F": draw(1); break; // Use this if you want to try removing the optimization
             default: // No operation
         }
     }
+    // Finish rendering the last segment if needed
+    if (currSegmentLength > 0) {
+        draw(currSegmentLength);
+    }
+
+    console.log(`Generated ${meshes.length} meshes.`);
     return meshes;
 }
 

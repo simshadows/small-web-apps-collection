9c7ab325e030bef38104564360f5986c318429be
simshadows <contact@simshadows.com>
2022-03-14 13:59:38 +1100

    [tictactoe] Implemented start button

--- apps-toolchainless/learning-and-experimental/2022-03-14_top-tictactoe/index.js
+++ apps-toolchainless/learning-and-experimental/2022-03-14_top-tictactoe/index.js
@@ -1,10 +1,68 @@
-let state;
-
-function resetState() {
-    state = {
-        // Nothing for now
+const txt = document.createTextNode.bind(document);
+function element(tagName, attributes={}) {
+    const elem = document.createElement(tagName);
+    for (const [k, v] of Object.entries(attributes)) {
+        switch (k) {
+            case "class":
+                elem.classList.add(...((v instanceof Array) ? v : [v]));
+                break;
+            default:
+                elem.setAttribute(k, v);
+        }
     }
+    return elem;
 }
 
-resetState();
+const state = {
+    reset: function() {
+        this.mode = "start"; // "start" | "playing"
+        this.gb = new GameBoard();
+    },
+    startGame: function() {
+        console.assert(this.mode === "start");
+        this.mode = "playing";
+    },
+};
+
+function GameBoard() {
+    return {
+        // grid represents a 3x3 board in row-major:
+        //      [ 0, 1, 2,
+        //        3, 4, 5,
+        //        6, 7, 8 ]
+        grid: new Array(9).fill(null),
+    };
+}
+
+const render = (()=>{
+    const playAreaElement = document.querySelector("#play-area");
+    const bottomBarElement = document.querySelector("#bottom-bar");
+
+    function renderPlaying() {
+        console.log("UNIMPLEMENTED");
+    }
+
+    function renderStart() {
+        const startButtonElem = bottomBarElement.appendChild(element("div", {class: "button"}));
+        startButtonElem.appendChild(txt("Start Game!"))
+        startButtonElem.addEventListener("click", () => state.startGame());
+    }
+
+    return function() {
+        console.log(state);
+
+        //playAreaElement.innerHTML = "";
+        bottomBarElement.innerHTML = "";
+        switch (state.mode) {
+            case "start":   renderStart();   break;
+            case "playing": renderPlaying(); break;
+            default: throw `Unknown state ${state.mode}`;
+        }
+    }
+})();
+
+document.addEventListener("click", render);
+
+state.reset();
+render();
 
